200ca8bf18881d7889f49cadd35bc5cabc0174fd
Bartek Gladecki <bgladecki@gmail.com>
2016-12-11 16:48:07 +0100

    typechange lol

--- symlinks/config.fish
+++ symlinks/config.fish
@@ -1,1 +1,91 @@
-/home/b/.config/fish/config.fish
+rvm default
+
+function v;vim $argv;end
+function e;emacs -nw $argv;end
+
+export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+function fish_greeting
+  #echo " -> you were chosen not because of who you are but because of who you can become." | lolcat
+  echo
+  /opt/gsp-mnemo | lolcat
+  echo
+end
+
+function headphones
+   amixer -c 1 set Headphone on;amixer -c 1 set Headphone 100%
+end
+
+function ll
+  /bin/ls --color=auto -l $argv
+end
+
+function la
+  /bin/ls --color=auto -A $argv
+end
+
+function l
+  /bin/ls --color=auto -CF $argv
+end
+
+function cdk
+  cd ~/code/dlog/backend
+end
+
+function cdl
+  cd ~/code/dlog/frontend
+end
+
+function cdm
+  cd ~/code/1op-elixir-vuejs
+end
+
+function mkdircd
+  mkdir $argv
+  cd $argv
+end
+
+function dropbox_encrypted_mount
+  cryfs ~/Dropbox/encrypted_do_not_touch ~/Dropbox_encrypted
+end
+
+function aptgetupdateshit
+  sudo apt update
+  sudo apt dist-upgrade
+end
+
+
+set fish_git_dirty_color red
+set fish_git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_diff (git diff)
+
+  if test -n "$git_diff"
+    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+  else
+    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+function ruby_version
+  echo (set_color cyan)(ruby --version | awk '{print $2}' | grep -ohE '[0-9]\.[0-9]\.[0-9]')(set_color normal)
+end
+
+function whoami_and_hostname
+  echo (set_color red)(whoami)'@'(hostname|cut -d . -f 1)(set_color normal)
+end
+
+function fish_prompt
+  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
+  printf '%s %s ' (whoami_and_hostname) (ruby_version)
+  if test -n "$git_dir"
+    printf '%s%s:%s%s' (set_color $fish_color_cwd) (prompt_pwd) (parse_git_branch) (set_color normal)
+  else
+    printf '%s%s%s' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  end
+
+  printf '%s > %s' (set_color blue) (set_color normal)
+end
--- symlinks/openbox/autostart.sh
+++ symlinks/openbox/autostart.sh
@@ -1,1 +1,13 @@
-/home/b/.config/openbox/autostart.sh
+caps_to_esc &
+tint2 &
+#hsetroot -solid '#0D0D0D'
+nitrogen --restore
+xfce4-power-manager &
+compton &
+dropbox start -i &
+volti &
+sleep 1; conky &
+synclient VertEdgeScroll=0 &
+amixer -c 1 set Headphone on;amixer -c 1 set Headphone 100% &
+
+xinput set-button-map 11 1 2 3 5 4 7 6 9 8 10 11 12

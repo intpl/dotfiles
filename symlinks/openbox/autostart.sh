4dd090d2ab0e9de280e364b2853ecaa797d207f8
Bartek Gladecki <bgladecki@gmail.com>
2017-05-26 11:27:50 +0200

    iaosjdiajsidoasd

--- symlinks/openbox/autostart.sh
+++ symlinks/openbox/autostart.sh
@@ -8,7 +8,7 @@ xfce4-volumed &
 dropbox start -i &
 #sleep 1; conky &
 synclient VertEdgeScroll=0 &
-amixer -c 1 set Headphone on;amixer -c 1 set Headphone 100% &
+#amixer -c 1 set Headphone on;amixer -c 1 set Headphone 100% &
 nm-applet &
 
 #xinput set-button-map 11 1 2 3 5 4 7 6 9 8 10 11 12

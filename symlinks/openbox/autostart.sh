8a3434ad44b8925a3f5f31c3d8a679a3d90c4d89
Bartek Gładecki <bgladecki@gmail.com>
2017-11-14 18:47:14 +0100

    vimrc cleanup

--- symlinks/openbox/autostart.sh
+++ symlinks/openbox/autostart.sh
@@ -9,6 +9,7 @@ dropbox start -i &
 synclient VertEdgeScroll=0 &
 #amixer -c 1 set Headphone on;amixer -c 1 set Headphone 100% &
 nm-applet &
+screenlets-daemon &
 sleep 1; conky &
 
 #xinput set-button-map 11 1 2 3 5 4 7 6 9 8 10 11 12
